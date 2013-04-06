245e9e941dfb7f075855d371c0e11baa7a9dde92
Derek A. Rhodes <physci@gmail.com>
2013-04-05 20:39:37 -0400

    removes dummy section, alters package name

--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"log"
-	"flag"
-	"os/exec"
-	"os"
-	"fmt"
-	"errors"
 	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
 	"strings"
 )
 
 var (
 	pkgNameMsg = "The package name used for the generated code."
-	pkgName = flag.String("pkg", "", pkgNameMsg)
+	pkgName    = flag.String("pkg", "", pkgNameMsg)
 
 	typeNameMsg = "The name the type to be inserted"
-	typeName = flag.String("type", "", typeNameMsg)
+	typeName    = flag.String("type", "", typeNameMsg)
 
-	genericNameMsg = "The name of the generic template"
-	genericName = flag.String("gen", "", genericNameMsg)
+	genericNameMsg = "The name of the generic template container type"
+	genericName    = flag.String("gen", "", genericNameMsg)
+
+	// listContainersMsg = "List the available container types"
+	// listContainers = flag.String("list", "", listContainersMsg)
 )
 
 func ParseFlags() {
 	flag.Parse()
-
-	if !flag.Parsed() {
-		flag.Usage()
-	}
 
 	if *pkgName == "" {
 		log.Println("You must supply a package name")
@@ -45,14 +45,15 @@ func ParseFlags() {
 	}
 }
 
+const URL = "https://raw.github.com/drhodes/gts/master/%s/%s.go"
+
 func BuildUrl(s string) string {
-	url := "https://raw.github.com/drhodes/gts/master/%s/%s.go"
-	url = fmt.Sprintf(url, s, s)
+	url := fmt.Sprintf(URL, s, s)
 	return url
 }
 
 func PageOk(url string) error {
-	cmd := exec.Command (		
+	cmd := exec.Command(
 		"curl",
 		"-4",
 		`--write-out`,
@@ -77,18 +78,18 @@ func PageOk(url string) error {
 	return nil
 }
 
-
 func GetTemplate(s string) (string, error) {
 	url := BuildUrl(s)
-	
-	err := PageOk(url) 
+
+	err := PageOk(url)
 	if err != nil {
 		msg := fmt.Sprintf("Fetching template failed:\n")
 		return "", errors.New(msg + err.Error())
 	} else {
-		log.Println("Template Found: ", url)
+		msg := "Template Found: " + url + "\n"
+		os.Stderr.Write([]byte(msg))
 	}
-	
+
 	log.Println("Fetching: " + url)
 	// so I think github has misconfigured ipv6, it seems to be timing out
 	// I don't know how to force http.Get() to resolve only ipv4
@@ -105,10 +106,52 @@ func GetTemplate(s string) (string, error) {
 	return out.String(), nil
 }
 
-
 func Generify(template string) string {
-	// TODO: buildin for multi parameter generics when that's needed
+	// TODO: build multi parameter generics when that's needed
 	return strings.Replace(template, `𝞃`, *typeName, -1)
+}
+
+func ReplacePackageName(template string) (string, error) {
+	// http://regoio.herokuapp.com/ <- highly recommended :)
+	pattern := `package ([\pL_])+([\pL\pN_])*`
+	re := regexp.MustCompile(pattern)
+	ends := re.FindStringIndex(template)
+
+	if ends == nil {
+		msg := "package declaration not found in template"
+		return "", errors.New(msg)
+	}
+
+	head := template[0:ends[0]]
+	tail := template[ends[1]:len(template)]
+	mid := "package " + (*pkgName)
+
+	return head + mid + tail, nil
+}
+
+// remove every line in the dummy section
+// what is the dummy section you ask?
+// it's just a part of the template that implements
+// a fake type so the template can compile and be tested.
+func RemoveDummySection(template string) string {
+	lines := strings.Split(template, "\n")
+	result := []string{}
+
+	capturing := true
+	for n := range lines {
+		if strings.Contains(lines[n], "dummy start") {
+			capturing = false
+		}
+
+		if capturing {
+			result = append(result, lines[n])
+		}
+
+		if strings.Contains(lines[n], "dummy end") {
+			capturing = true
+		}
+	}
+	return strings.Join(result, "\n")
 }
 
 func main() {
@@ -119,10 +162,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(Generify(template))
+	template = Generify(template)
 
-	// need to change the package name
-	// need to remove the dummy lines
+	template, err = ReplacePackageName(template)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-
+	template = RemoveDummySection(template)
+	fmt.Println(template)
 }
